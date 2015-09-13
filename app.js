e90d9d89df2d8b81f9d4aab0c6de109007a57999
tianyingchun <tianyingchun@outlook.com>
2015-09-13 23:55:12 +0800

    update test sample for react-sample app

--- app.js
+++ app.js
@@ -31,20 +31,23 @@ app.use("/examples", cors(), express.static(path.join(__dirname, './examples')))
 // The testing purpose for  `react-sample`
 app.get("/workspace/list", function (req, res) {
   var initialState = {
-    "workspaces": [{
-      "id": 1,
-      "name": "workspace name 1"
-    }, {
-      "id": 2,
-      "name": "workspace name 2"
+    workspaces: {
+      isLoading: false,
+      list: [{
+        "id": 1,
+        "name": "workspace name 1"
+      }, {
+        "id": 2,
+        "name": "workspace name 2"
+      }, {
+        "id": 3,
+        "name": "workspace name 3"
+      }, {
+        "id": 4,
+        "name": "workspace name 4"
+      }]
     },
-    {
-      "id": 3,
-      "name": "workspace name 3"
-    },{
-      "id": 4,
-      "name": "workspace name 4"
-    }]
+    workspaceOthers: ''
   };
   var html = '<!DOCTYPE html>' +
     ' <html>' +
@@ -54,14 +57,14 @@ app.get("/workspace/list", function (req, res) {
     '  <meta http-equiv="Cache-Control" content="no-siteapp">' +
     '  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">' +
     '  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">' +
-    '  <link rel="stylesheet" type="text/css" href="http://172.16.233.137:3000/public/workspace/wslist/bundle.css">' +
+    '  <link rel="stylesheet" type="text/css" href="http://localhost:3000/public/workspace/wslist/bundle.css">' +
     '</head>' +
     '  <body>' +
     '    <script>window.__INITIAL_STATE__ = ' + JSON.stringify(initialState) + '</script>' +
     '    <div id="react-view"></div>' +
-    '    <script src="http://172.16.233.137:3000/public/browser-polyfill.js"></script>' +
-    '    <script src="http://172.16.233.137:3000/public/reactkits.js"></script>' +
-    '    <script src="http://172.16.233.137:3000/public/workspace/wslist/bundle.js"></script>' +
+    '    <script src="http://localhost:3000/public/browser-polyfill.js"></script>' +
+    '    <script src="http://localhost:3000/public/reactkits.js"></script>' +
+    '    <script src="http://localhost:3000/public/workspace/wslist/bundle.js"></script>' +
     '  </body>' +
     '</html>';
 
